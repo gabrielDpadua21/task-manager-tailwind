b5e15595ff8b4d6dab7c1521e502dd02a6059234
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-28 01:37:21 -0300

    feat: ✨ create and testing task model

---
+++ src/model/Task.ts
@@ -0,0 +1,43 @@
+export default class Task {
+    private _id: number;
+    private _description: string;
+    private _status: boolean;
+
+    constructor(id: number, description: string, status = false) {
+        this._id = id
+        this._description = description
+        this._status = status
+    }
+
+    public static create(id: number, description: string) {
+        return new Task(id, description)
+    }
+
+    public static createFinished(id: number, description: string) {
+        return new Task(id, description, true)
+    }
+
+    public get id() {
+        return this._id;
+    }
+
+    public get description() {
+        return this._description;
+    }
+
+    public get status() {
+        return this._status;
+    }
+
+    public toogleStatus() {
+        return this.status ? this.active() : this.finished()
+    }
+
+    public finished() {
+        return Task.createFinished(this._id, this._description)
+    }
+
+    public active() {
+        return Task.create(this._id, this._description)
+    }
+ }
--- src/pages/teste.tsx
+++ src/pages/teste.tsx
@@ -1,11 +1,16 @@
+import Task from '@/model/Task'
 import '../styles/globals.css'
 
 export default function Text() {
+
+    let task: Task = new Task(1, 'Exemple')
+    task = task.finished()
+
     return (
       <div className='flex flex-col h-screen text-white items-center justify-center bg-gradient-to-tr from-purple-500 to-pink-600'>
-        <span className='text-40px'>1</span>
-        <span className='text-40px'>2</span>
-        <span className='text-40px'>3</span>
+        <span className='text-40px'>{task.id}</span>
+        <span className='text-40px'>{task.description}</span>
+        <span className='text-40px'>{task.status ? 'Yes' : 'No'}</span>
       </div>
     )
   }
