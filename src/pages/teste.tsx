4398167b20dc0adcbebd5a8e33a0e07db2b16fee
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-06-28 01:17:44 -0300

    feat: ✨ firt exemple with tailwind

---
+++ src/pages/teste.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+export default function Text() {
+    return (
+      <div className='flex flex-col h-screen text-white items-center justify-center bg-gradient-to-tr from-purple-500 to-pink-600'>
+        <span className='text-40px'>1</span>
+        <span className='text-40px'>2</span>
+        <span className='text-40px'>3</span>
+      </div>
+    )
+  }
